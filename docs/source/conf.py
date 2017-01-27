861b1a4d744e7a3eb52b6225dbb8bb4846b34e8e
Iain Brown <codeiain@outlook.com>
2017-01-27 23:39:13 +0000

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,6 +18,12 @@
 #
 import os
 import sys
+import sphinx_rtd_theme
+
+html_theme = "sphinx_rtd_theme"
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 sys.path.insert(0, "/Users/iain/Dev/RaspberryPiBot/src")
 # -- General configuration ------------------------------------------------
 
@@ -89,7 +95,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
